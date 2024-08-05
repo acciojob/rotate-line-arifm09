9f3d9f49c9fc81ae5b608fe30e9106cdba0e24b1
arifm09 <86922587+arifm09@users.noreply.github.com>
2024-08-05 10:55:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let angle = 0;
+
+let line = document.getElementById("line");
+
+ let intervalId = setInterval(() =>{
+	angle += 2;
+    line.style.transform = `rotate(${angle}deg)`;
+},20);
+
+
